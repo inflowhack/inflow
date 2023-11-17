7ac53ad6771062fbec7d7dfdc728fc4969aeb2b3
Aya <aya.belarbi16@gmail.com>
2023-11-17 16:17:57 +0300

    init frontend

---
+++ inflow-frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+
+      </div>
+      <h1>Inflow</h1>
+
+    </>
+  )
+}
+
+export default App
---
+++ inflow-frontend/src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Routes> 
+
+
+        <Route path="/" element={<App/>}/> 
+ 
+
+    </Routes> 
+    </BrowserRouter>
+
+  </React.StrictMode>,
+)

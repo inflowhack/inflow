8ae71f8fee2b8bd448faaebab6dd4f404cedc064
Danny Pr <47109543+dannpr@users.noreply.github.com>
2023-11-18 18:44:20 +0300

    Solve deps

---
+++ inflow-frontend/src/api/onramp-unlimit-sdk.tsx
@@ -0,0 +1,25 @@
+import { GateFiDisplayModeEnum, GateFiSDK } from "@gatefi/js-sdk";
+//import crypto from 'crypto'
+
+// Function to create a new embed SDK instance
+export const createEmbedSdkInstance = () => {
+/*   const randomString = crypto.randomBytes(32).toString("hex");
+ */   
+  try {
+    const embedInstanceSDK = new GateFiSDK({
+      merchantId: import.meta.env.VITE_MERCHANT, // pv 
+      displayMode: GateFiDisplayModeEnum.Embedded,
+      nodeSelector: "#overlay-button",
+      isSandbox: true,
+      walletAddress: import.meta.env.VITE_WALLET, // pv
+      styles: {
+        type: "dark",
+      },
+    })
+
+    return embedInstanceSDK;
+  } catch (error) {
+    console.error("Error creating GateFi SDK instance:", error);
+    return null;
+  }
+};
